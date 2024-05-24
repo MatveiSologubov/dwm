a708bc7760e36a7b91549d3a0696f89a486a83ed
Matvei Sologubov <matthew.sologubov@gmail.com>
2024-05-25 02:04:32 +0300

    update colors

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "MononokiNerdFontPropo:size=14" };
 static const char dmenufont[]       = "MononokiNerdFontPropo:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#080705";
+static const char col_gray2[]       = "#2B2D42";
+static const char col_gray3[]       = "#8D99AE";
+static const char col_gray4[]       = "#EDF2F4";
+static const char col_cyan[]        = "#D90429";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
