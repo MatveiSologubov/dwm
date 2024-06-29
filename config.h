8c8c1703e1d272ec962f37a1c8e1eec46f1bb6f6
Matvei Sologubov <matthew.sologubov@gmail.com>
2024-06-29 21:38:31 +0000

    feat: Add shortcut for Brave Shortcut for Brave is: Master + B

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = roficmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		XK_q,      spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_b,      spawn,	   {.v = (const char*[]){ "brave", "NULL" } } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
