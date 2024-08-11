0f182102c75278b728971830ba0b6ece4a7c60f4
Matvei Sologubov <matthew.sologubov@gmail.com>
2024-08-11 20:51:53 +0000

    feat: Change font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MononokiNerdFontPropo:size=14" };
-static const char dmenufont[]       = "MononokiNerdFontPropo:size=14";
+static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12:antialias=true" };
+static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=12:antialias=true";
 static const char col_gray1[]       = "#080705";
 static const char col_gray2[]       = "#2B2D42";
 static const char col_gray3[]       = "#8D99AE";
