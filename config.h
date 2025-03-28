fd10a37b90343f0d623e7d7a710e30cd316c0cd5
Matvei Sologubov <matthew.sologubov@gmail.com>
2025-03-28 15:23:28 +0300

    fix: Add accidentally removed shortcut for LO-Writer

--- config.h
+++ config.h
@@ -69,6 +69,7 @@ static const Key keys[] = {
 	{ MODKEY,						XK_q,      spawn,          {.v = termcmd } },
 	{ MODKEY,						XK_b,      spawn,	       {.v = (const char*[]){ "brave", NULL } } },
 	{ MODKEY,						XK_o,      spawn,	       {.v = (const char*[]){ "obsidian", NULL } } },
+	{ MODKEY,						XK_w,      spawn,		   {.v = (const char*[]){ "lowriter", NULL } } },
 	{ MODKEY,						XK_v,      spawn,		   {.v = (const char*[]){ "pavucontrol", NULL } } },
 	{ MODKEY,						XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,						XK_k,      focusstack,     {.i = -1 } },
